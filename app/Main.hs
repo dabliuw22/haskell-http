ae413b44ac504131cf25c80402007332f2a65e5e
Will Leyton <wleylop22@gmail.com>
2020-07-06 17:46:31 -0500

    Update

--- app/Main.hs
+++ app/Main.hs
@@ -16,8 +16,7 @@ import System.Environment (lookupEnv)
 
 main :: IO ()
 main = do
-  conf <- load
-  pool <- DB.create conf
+  pool <- DB.create
   dir <- lookupEnv "DB_MIGRATION_DIR"
   migration <- M.migrate pool (fromMaybe "db/migrations" dir)
   let f1 = SERVICE.findAll (REPO.findAll pool)
@@ -30,18 +29,3 @@ proxy :: Proxy HTTP.ProductRoute
 proxy = Proxy
 
 --server :: Application
-
-load :: MonadIO m => m DB.PgConfig
-load = do
-  host' <- liftIO $ lookupEnv "DB_HOST"
-  port' <- liftIO $ lookupEnv "DB_PORT"
-  user' <- liftIO $ lookupEnv "DB_USER"
-  pass' <- liftIO $ lookupEnv "DB_PASS"
-  name' <- liftIO $ lookupEnv "DB_NAME"
-  return DB.PgConfig {
-           DB.host = fromMaybe "localhost" host',
-           DB.port = read (fromMaybe "5432" port') :: Integer,
-           DB.user = fromMaybe "haskell" user',
-           DB.pass = fromMaybe "haskell" pass',
-           DB.database = fromMaybe "haskell_db" name'
-         }
--- src/Adapter/Postgres/Config/PostgresConfig.hs
+++ src/Adapter/Postgres/Config/PostgresConfig.hs
@@ -1,8 +1,10 @@
 module Adapter.Postgres.Config.PostgresConfig (PgConfig(..), create, destroy) where
 
 import qualified Database.PostgreSQL.Simple as PG
+import Data.Maybe (fromMaybe)
 import Data.Pool
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import System.Environment (lookupEnv)
 
 data PgConfig =
    PgConfig {
@@ -12,9 +14,26 @@ data PgConfig =
     pass :: String,
     database :: String
    } deriving Eq
+
+loadPgConfig :: MonadIO m => m PgConfig
+loadPgConfig = do
+  host' <- liftIO $ lookupEnv "DB_HOST"
+  port' <- liftIO $ lookupEnv "DB_PORT"
+  user' <- liftIO $ lookupEnv "DB_USER"
+  pass' <- liftIO $ lookupEnv "DB_PASS"
+  name' <- liftIO $ lookupEnv "DB_NAME"
+  return PgConfig {
+           host = fromMaybe "localhost" host',
+           port = read (fromMaybe "5432" port') :: Integer,
+           user = fromMaybe "haskell" user',
+           pass = fromMaybe "haskell" pass',
+           database = fromMaybe "haskell_db" name'
+         }
    
-create :: MonadIO m => PgConfig -> m (Pool PG.Connection)
-create conf = liftIO $ createPool (connection conf) close 1 10 15
+create :: MonadIO m => m (Pool PG.Connection)
+create = do
+  conf <- loadPgConfig
+  liftIO $ createPool (connection conf) close 1 10 15
 
 destroy :: MonadIO m => Pool PG.Connection -> m ()
 destroy pool = liftIO $ destroyAllResources pool
