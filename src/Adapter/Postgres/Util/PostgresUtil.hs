b928f8f71f7936aa6235bfb18969945b1d3a96f0
Will Leyton <wleylop22@gmail.com>
2020-07-09 17:31:47 -0500

    Update

--- src/Adapter/Postgres/Util/PostgresUtil.hs
+++ src/Adapter/Postgres/Util/PostgresUtil.hs
@@ -28,84 +28,84 @@ class PostgresUtil m where
     -> b -> m (Bool)
 
 instance PostgresUtil IO where
-  queryOne pool q b = do
-    logger $ \logEnv -> do
-      runKatipContextT logEnv () namespace $ do
-        $(logTM) InfoS $ "Query One Start..."
-    result <- queryOne' pool q b `catch` \e -> handleSqlError e namespace
-    logger $ \logEnv -> do
-      runKatipContextT logEnv () namespace $ do
-        $(logTM) InfoS $ "Query One End..."
-    return result
-    where
-      namespace = "query-one"
-  queryListWithoutParams pool q = do
-    logger $ \logEnv -> do
-      runKatipContextT logEnv () namespace $ do
-        $(logTM) InfoS $ "Query List Without Params Start..."
-    result <- queryListWithoutParams' pool q `catch` \e -> handleSqlError e namespace
-    logger $ \logEnv -> do
-      runKatipContextT logEnv () namespace $ do
-        $(logTM) InfoS $ "Query List Without Params End..."
-    return result
-    where
-      namespace = "query-list-without-params"
-  queryList pool q b = do
-    logger $ \logEnv -> do
-      runKatipContextT logEnv () namespace $ do
-        $(logTM) InfoS $ "Query List Start..."
-    result <- queryList' pool q b `catch` \e -> handleSqlError e namespace
-    logger $ \logEnv -> do
-      runKatipContextT logEnv () namespace $ do
-        $(logTM) InfoS $ "Query List End..."
-    return result
-    where
-      namespace = "query-list"
-  command pool c b = do
-    logger $ \logEnv -> do
-      runKatipContextT logEnv () namespace $ do
-        $(logTM) InfoS $ "Command Start..."
-    result <- command' pool c b `catch` \e -> handleSqlError e namespace
-    logger $ \logEnv -> do
-      runKatipContextT logEnv () namespace $ do
-        $(logTM) InfoS $ "Command End..."
-    return result
-    where
-      namespace = "command"
+  queryOne pool q b = queryOne' pool q b
+  queryListWithoutParams pool q = queryListWithoutParams' pool q
+  queryList pool q b = queryList' pool q b
+  command pool c b = command' pool c b 
 
 queryOne' :: (MonadIO m, MonadThrow m, FromRow a, ToRow b)
   => Pool PG.Connection
   -> PG.Query -> b -> m (Maybe a)
 queryOne' p' q' b = do
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) InfoS $ "Query One Start..."
   result <- liftIO $ withResource p' (\conn -> PG.query conn q' b)
+    `catch` \e -> handleSqlError e namespace
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) InfoS $ "Query One End..."
   case result of
         (h: _) -> return $ Just h
         _      -> return Nothing
+  where
+    namespace = "query-one"
 
 queryList' :: (MonadIO m, MonadThrow m, FromRow a, ToRow b)
   => Pool PG.Connection
   -> PG.Query -> b -> m [a]
-queryList' p' q' b = liftIO $ withResource p' (\conn -> PG.query conn q' b)
+queryList' p' q' b = do
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) InfoS $ "Query List Start..."
+  result <- liftIO $ withResource p' (\conn -> PG.query conn q' b)
+    `catch` \e -> handleSqlError e namespace
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) InfoS $ "Query List End..."
+  return result
+  where
+    namespace = "query-list"
 
 queryListWithoutParams' :: (MonadIO m, MonadThrow m, FromRow a)
   => Pool PG.Connection
   -> PG.Query -> m [a]
-queryListWithoutParams' p' q' = liftIO $ withResource p' (`PG.query_` q')
+queryListWithoutParams' p' q' = do
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) InfoS $ "Query List Without Params Start..."
+  result <- liftIO $ withResource p' (`PG.query_` q') 
+    `catch` \e -> handleSqlError e namespace
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) InfoS $ "Query List Without Params End..."
+  return result
+  where
+    namespace = "query-list-without-params"
 
 command' :: (MonadIO m, MonadThrow m, ToRow b)
   => Pool PG.Connection
   -> PG.Query -> b -> m Bool
 command' p' q' b = do
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) InfoS $ "Command Start..."
   result <- liftIO $ withResource p' (\conn -> PG.execute conn q' b)
+    `catch` \e -> handleSqlError e namespace
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) InfoS $ "Command End..."
   return (result > 0)
+  where
+    namespace = "command"
 
 newtype PostgresException = PostgresException String deriving (Show, Typeable)
 
 instance Exception PostgresException
 
-handleSqlError :: PG.SqlError -> Namespace -> IO a
+handleSqlError :: (MonadIO m, MonadThrow m) => PG.SqlError -> Namespace -> m a
 handleSqlError e @ (PG.SqlError _ _ m _ _) namespace = do
-  logger $ \logEnv -> do
-        runKatipContextT logEnv () namespace $ do
-          $(logTM) ErrorS $ "Error..."
+  liftIO $ logger $ \logEnv -> do
+    runKatipContextT logEnv () namespace $ do
+      $(logTM) ErrorS $ "Error..."
   throwM $ PostgresException (show e)
