0a7e19e7fe35497d04ed4d48e2b96e90a2ccf8f2
Will Leyton <wleylop22@gmail.com>
2020-07-20 16:41:26 -0500

    Add Product Eq

--- src/Domain/Products.hs
+++ src/Domain/Products.hs
@@ -6,11 +6,11 @@ import Data.Text (Text)
 import Data.Typeable
 import Data.Time (ZonedTime)
 
-newtype ProductId = ProductId { id :: Text } deriving Show
+newtype ProductId = ProductId { id :: Text } deriving  (Show, Eq)
 
-newtype ProductName = ProductName { name :: Text} deriving Show
+newtype ProductName = ProductName { name :: Text} deriving (Show, Eq)
 
-newtype ProductStock = ProductStock { stock :: Double } deriving Show
+newtype ProductStock = ProductStock { stock :: Double } deriving  (Show, Eq)
 
 newtype ProductCreatedAt = ProductCreatedAt { createdAt :: ZonedTime } deriving Show
 
@@ -21,6 +21,10 @@ data Product =
     productStock :: ProductStock,
     productCreatedAt :: ProductCreatedAt
   } deriving Show
+
+instance Eq Product where
+  (==) p1 p2 = productId p1 == productId p2
+  (/=) p1 p2 = productId p1 /= productId p2
   
 newtype ProductException = ProductException String deriving (Show, Typeable)
 
