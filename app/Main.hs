3984c98919183eb60bb618656fc398d5681bd95a
Will Leyton <wleylop22@gmail.com>
2020-07-20 13:13:26 -0500

    Add API type

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,23 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
 module Main where
 
 import qualified Application.Products as SERVICE
 import qualified Adapter.Http.Products as HTTP
+import Adapter.Katip.Logger (logger)
 import qualified Adapter.Postgres.Config.PostgresConfig as DB
 import qualified Adapter.Postgres.Migration.PostgresMigration as M
 import qualified Adapter.Postgres.Products as REPO
 import Data.Maybe (fromMaybe)
 import Data.Proxy
+import Katip
 import Network.Wai (Application)
 import Network.Wai.Handler.Warp (run)
 import Servant
 import System.Environment (lookupEnv)
-import Katip
-import Adapter.Katip.Logger (logger)
 
 main :: IO ()
 main = do
@@ -29,13 +31,17 @@ main = do
   let f1 = SERVICE.findAll (REPO.findAll pool)
       f2 = SERVICE.findById (REPO.findById pool)
       f3 = SERVICE.create (REPO.create pool)
-      server = serve proxy $ HTTP.routes f1 f2 f3
+      server = serve proxy $ routes f1 f2 f3
   logger $ \logEnv -> do
     runKatipContextT logEnv () "server-start" $ do
       $(logTM) InfoS "Start Server..."
   run port' server
+
+type API = HTTP.ProductRoute
+
+routes = HTTP.routes
   
-proxy :: Proxy HTTP.ProductRoute
+proxy :: Proxy API
 proxy = Proxy
 
 --server :: Application
