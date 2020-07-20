4612b6a6817c1e50cec80c6c59f43815b5970074
Will Leyton <wleylop22@gmail.com>
2020-07-20 11:10:14 -0500

    Update HTTP

--- src/Adapter/Http/GetProducts.hs
+++ src/Adapter/Http/GetProducts.hs
@@ -8,10 +8,8 @@ import Application.Products
 import Control.Exception (try)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Aeson
-import Data.Text (Text, pack)
-import Data.Time (ZonedTime, getZonedTime)
-import Data.UUID.V1 (nextUUID)
-import qualified Data.UUID as UUID
+import Data.Text (Text)
+import Data.Time (ZonedTime)
 import qualified Domain.Products as DOMAIN
 import GHC.Generics (Generic)
 import Servant
@@ -20,21 +18,12 @@ import Data.Maybe (fromJust)
 type ProductRoute =
   "products" :> (
     Get '[JSON] [GetProductDto] :<|>
-    Capture "id" Text :> Get '[JSON] GetProductDto :<|>
-    ReqBody '[JSON] CreateProductDto :> PostCreated '[JSON] ()
+    Capture "id" Text :> Get '[JSON] GetProductDto
   ) -- or 
   {-
   "products" :> Get '[JSON] [GetProductDto] :<|>
-  "products" :> Capture "id" Text :> Get '[JSON] GetProductDto :<|>
-  "products" :> ReqBody '[JSON] CreateProductDto :> PostCreated '[JSON] ()
+  "products" :> Capture "id" Text :> Get '[JSON] GetProductDto
   -}
-
-
-data CreateProductDto =
-  CreateProductDto {
-    name :: Text,
-    stock :: Double
-  } deriving (Generic, Show)
   
 data GetProductDto = 
   GetProductDto {
@@ -46,13 +35,10 @@ data GetProductDto =
 
 instance ToJSON GetProductDto
 
-instance FromJSON CreateProductDto
-
 routes :: IO [DOMAIN.Product]
   -> (Text -> IO (Maybe DOMAIN.Product))
-  -> (DOMAIN.Product -> IO ())
   -> Server ProductRoute
-routes f1 f2 f3 = allProducts f1 :<|> oneProduct f2 :<|> createProduct f3
+routes f1 f2 = allProducts f1 :<|> oneProduct f2 
 
 oneProduct :: (Text -> IO (Maybe DOMAIN.Product)) -> Text -> Handler GetProductDto
 oneProduct f' id' = do
@@ -75,20 +61,6 @@ allProducts f' =  do
                             errBody = "Error Get All Products"
                           }
 
-createProduct :: (DOMAIN.Product -> IO ()) -> CreateProductDto -> Handler ()
-createProduct f' dto' = do
-  uuid <- liftIO nextUUID
-  let uuid' = pack (UUID.toString (fromJust uuid))
-  createdAt <- liftIO getZonedTime
-  result <- liftIO $ try (f' (from dto' uuid' createdAt))
-  case result of
-      Right v -> return v
-      Left e  -> case e of
-                      DOMAIN.ProductException s -> throwError 
-                        err400 { 
-                          errBody = "Error Create Product" 
-                        }
-
 to :: DOMAIN.Product -> GetProductDto
 to p =
   GetProductDto {
@@ -96,13 +68,4 @@ to p =
     product_name = DOMAIN.name (DOMAIN.productName p),
     product_stock = DOMAIN.stock (DOMAIN.productStock p),
     product_created_at = DOMAIN.createdAt (DOMAIN.productCreatedAt p)
-  }
-
-from :: CreateProductDto -> Text -> ZonedTime -> DOMAIN.Product
-from dto uuid createdAt =
-  DOMAIN.Product {
-    DOMAIN.productId = DOMAIN.ProductId uuid,
-    DOMAIN.productName = DOMAIN.ProductName (name dto),
-    DOMAIN.productStock = DOMAIN.ProductStock (stock dto),
-    DOMAIN.productCreatedAt = DOMAIN.ProductCreatedAt createdAt
   }
