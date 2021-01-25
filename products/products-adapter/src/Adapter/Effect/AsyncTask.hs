c3f7b8cd77555f74b5c0b49bd441a6271cf73b08
Will Leyton <wleylop22@gmail.com>
2021-01-25 08:30:50 -0500

    Add AsyncTask

---
+++ products/products-adapter/src/Adapter/Effect/AsyncTask.hs
@@ -0,0 +1,15 @@
+module Adapter.Effect.AsyncTask (AsyncTask (..)) where
+
+import Control.Concurrent.Async (async, waitCatch)
+import Control.Monad.Catch (MonadThrow, throwM)
+
+class (Monad m, MonadThrow m) => AsyncTask m where
+  run :: m a -> m a
+
+instance AsyncTask IO where
+  run action = do
+    var <- async action
+    result <- waitCatch var
+    case result of
+      Right v -> return v
+      Left e -> throwM e
